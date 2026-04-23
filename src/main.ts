67dc9364f7cba9ec07fd6fc0d589a3d43bd141c4
Italo1994 <italohortiz2015@gmail.com>
2026-04-22 21:58:21 -0300

    feat: add process.on to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
+
+  process.on('uncaughtException', (err) => {
+    console.error('UNCAUGHT:', err);
+  });
+
+  process.on('unhandledRejection', (err) => {
+    console.error('UNHANDLED:', err);
+  });
 }
 bootstrap();
